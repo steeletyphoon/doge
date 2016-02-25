fc3ca6131ccd2cab425ac33a4396a4bd529090e7
Seth Steele <ssteele823@gmail.com>
2016-02-25 18:58:49 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -14,15 +14,33 @@ DogeDodge.Play.prototype = {
 
   preload : function () {
     this.load.image('background', 'assets/background.jpg');
+    this.load.spritesheet('dodger', 'assets/groom.png', 64,199,2);
   },
 
   create: function () {
     this.background = this.add.tileSprite(0,0,320,568,"background" );
-    this.background.autoScroll(0,-50);
+    this.background.autoScroll(0,50);
     this.background.scale.set(1);
+
+    this.dodger = this.add.sprite(160,518, 'dodger');
+    this.dodger.anchor.set(0.5,0.5);
+    
+    this.cursors = game.input.keyboard.createCursorKeys();
+
+  
   },
 
+
+
   update: function () {
+    if (this.cursors.left.isDown) {
+      this.dodger.x -= 10;
+    }
+    if (this.cursors.right.isDown) {
+      this.dodger.x += 10;
+    }
   }
 
 };
+
+
