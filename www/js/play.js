ab2284ff62f9a4dd31c42a96ee535f1e00d4d65f
Seth Steele <ssteele823@gmail.com>
2016-03-03 18:49:57 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -27,7 +27,7 @@ DogeDodge.Play.prototype = {
     this.dodger.anchor.set(0.5,0.5);
     
     this.fall = this.add.sprite(296 ,200, 'fall');
-    this.fall.anchor.set(3,3);
+    this.fall.anchor.set(3,4);
     this.fall.scale.set(.25);
 
     this.cursors = game.input.keyboard.createCursorKeys();
